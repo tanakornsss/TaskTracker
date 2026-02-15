df9d7d66dda89936fbebe42d04831df8f4abd6e1
tanakornsss <omtanakorn2550@gmail.com>
2026-02-15 17:14:38 +0700

    Fix layout issues

--- app/src/main/java/dev/tanakornsss/tasktracker/MainActivity.kt
+++ app/src/main/java/dev/tanakornsss/tasktracker/MainActivity.kt
@@ -3,6 +3,7 @@ package dev.tanakornsss.tasktracker
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
@@ -16,10 +17,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_landing)
 
         val toolbar = findViewById<MaterialToolbar>(R.id.topAppBar)
-
         ViewCompat.setOnApplyWindowInsetsListener(toolbar) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val landing = findViewById<ConstraintLayout>(R.id.landing)
+        ViewCompat.setOnApplyWindowInsetsListener(landing) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, 0, systemBars.right, systemBars.bottom)
             insets
         }
     }
